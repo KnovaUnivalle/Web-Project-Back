d3e3eb12f686e8ba326145a133b10b44b2c0b4f2
sebastian6551 <sebastian6551@live.com>
2023-05-13 20:15:25 -0500

    All models serializers

--- backend/core/api.py
+++ backend/core/api.py
@@ -1,9 +1,46 @@
 from rest_framework import viewsets, permissions
-from .serializers import AdminSerializer
-from .models import admin
+from .serializers import AdminSerializer, RoleSerializer, UserSerializer, ProductSerializer, StoreSerializer, SearchHistorySerializer, SuggestionSerializer
+from .models import admin, rol, user, product, store, searchHistory, suggestion
+
 
 # Indicates which queries could be made
 class AdminViewSet(viewsets.ModelViewSet):
     queryset = admin.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = AdminSerializer
+
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = rol.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RoleSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = user.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = product.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductSerializer
+
+
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = store.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StoreSerializer
+
+
+class SearchHistoryViewSet(viewsets.ModelViewSet):
+    queryset = searchHistory.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SearchHistorySerializer
+
+
+class SuggestionViewSet(viewsets.ModelViewSet):
+    queryset = suggestion.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SuggestionSerializer
