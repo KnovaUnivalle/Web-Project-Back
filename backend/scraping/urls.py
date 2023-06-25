fb4375a60c7b1fd5f195ab08a279bd10e3de7e96
Víctor Andrés Sapuyes <54079546+andresNeveu@users.noreply.github.com>
2023-06-24 23:40:42 -0500

    Update urls.py

--- backend/scraping/urls.py
+++ backend/scraping/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import searchProduct
 
 urlpatterns = [
-    path('', searchProduct, name='searchProduct'),
+    path('scraping/', searchProduct, name='searchProduct'),
 ]
